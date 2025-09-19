800483e50ce45e71c06b98d3614b4cea10502419
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-09-19 12:35:57 +0200

    Split pkg cmdlang-cmdliner-runner

---
+++ lib/cmdlang_cmdliner_runner/sub-pkg/cmdlang_cmdliner_runner.ml
@@ -0,0 +1,7 @@
+(*********************************************************************************)
+(*  pplumbing - Utility libraries to use with [pp]                               *)
+(*  SPDX-FileCopyrightText: 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*  SPDX-License-Identifier: MIT                                                 *)
+(*********************************************************************************)
+
+include Pplumbing_cmdlang_cmdliner_runner.Cmdlang_cmdliner_runner
