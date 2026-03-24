4c556b974bfb1fde2096689a5aeb74810bfb4c5e
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-24 10:00:10 +0100

    Use dune-build-info for --version

--- test/cram/main_climate.ml
+++ test/cram/main_climate.ml
@@ -4,9 +4,10 @@
 (*  SPDX-License-Identifier: MIT                                                 *)
 (*********************************************************************************)
 
-let () =
-  Cmdlang_climate_err_runner.run
-    Command_test.main
-    ~name:"main-climate"
-    ~version:"%%VERSION%%"
+let version =
+  match Build_info.V1.version () with
+  | None -> "n/a"
+  | Some v -> Build_info.V1.Version.to_string v
 ;;
+
+let () = Cmdlang_climate_err_runner.run Command_test.main ~name:"main-climate" ~version
