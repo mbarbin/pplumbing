c774ea5a15c2f772d1ee695b9f0377425f3ea149
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-31 10:29:59 +0200

    Add more modules to test stdlib

---
+++ src/stdlib/ref0.ml
@@ -0,0 +1,11 @@
+(*********************************************************************************)
+(*  pplumbing - Utility libraries to use with [pp]                               *)
+(*  SPDX-FileCopyrightText: 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*  SPDX-License-Identifier: MIT                                                 *)
+(*********************************************************************************)
+
+let set_temporarily t a ~f =
+  let x = !t in
+  t := a;
+  Fun.protect ~finally:(fun () -> t := x) f
+;;
